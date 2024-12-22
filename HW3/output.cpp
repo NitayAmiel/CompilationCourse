4636460e8afa0374ea2e5301ce218ea4a6a0f3e8
Mathmatician1 <nitay471.na@gmail.com>
2024-12-22 20:41:39 +0200

    after first steps, before adding member to Exp class

--- HW3/output.cpp
+++ HW3/output.cpp
@@ -142,4 +142,374 @@ namespace output {
         os << "---end global scope---" << std::endl;
         return os;
     }
+
+
+
+    MyVisitor::MyVisitor(){
+        std::vector<SymTableEntry> initial_vector;
+        this->sym_table.push_back(initial_vector);
+        this->insert_func("print", ast::BuiltInType::VOID, std::vector<ast::BuiltInType>({ast::BuiltInType::STRING}));
+        this->insert_func("printi", ast::BuiltInType::VOID, std::vector<ast::BuiltInType>({ast::BuiltInType::INT}));
+        this->offset_table.push_back(0);
+        
+    }
+
+    void MyVisitor::visit(ast::Num &node) {
+        
+    }
+
+    void MyVisitor::visit(ast::NumB &node) {
+        print_indented("NumB: " + std::to_string(node.value));
+    }
+
+    void MyVisitor::visit(ast::String &node) {
+        print_indented("String: " + node.value);
+    }
+
+    void MyVisitor::visit(ast::Bool &node) {
+        print_indented("Bool: " + std::string((node.value ? "true" : "false")));
+    }
+
+    void MyVisitor::visit(ast::ID &node) {
+        print_indented("ID: " + node.value);
+    }
+
+    void MyVisitor::visit(ast::BinOp &node) {
+        std::string op;
+
+        switch (node.op) {
+            case ast::BinOpType::ADD:
+                op = "+";
+                break;
+            case ast::BinOpType::SUB:
+                op = "-";
+                break;
+            case ast::BinOpType::MUL:
+                op = "*";
+                break;
+            case ast::BinOpType::DIV:
+                op = "/";
+                break;
+        }
+
+        print_indented("BinOp: " + op);
+
+        enter_child();
+        node.left->accept(*this);
+        leave_child();
+
+        enter_last_child();
+        node.right->accept(*this);
+        leave_child();
+    }
+
+    void MyVisitor::visit(ast::RelOp &node) {
+        std::string op;
+
+        switch (node.op) {
+            case ast::RelOpType::EQ:
+                op = "==";
+                break;
+            case ast::RelOpType::NE:
+                op = "!=";
+                break;
+            case ast::RelOpType::LT:
+                op = "<";
+                break;
+            case ast::RelOpType::LE:
+                op = "<=";
+                break;
+            case ast::RelOpType::GT:
+                op = ">";
+                break;
+            case ast::RelOpType::GE:
+                op = ">=";
+                break;
+        }
+
+        print_indented("RelOp: " + op);
+
+        enter_child();
+        node.left->accept(*this);
+        leave_child();
+
+
+        enter_last_child();
+        node.right->accept(*this);
+        leave_child();
+    }
+
+    void MyVisitor::visit(ast::Type &node) {
+        print_indented("Type: " + toString(node.type));
+    }
+
+    void MyVisitor::visit(ast::Cast &node) {
+        print_indented("Cast");
+
+        enter_child();
+        node.exp->accept(*this);
+        leave_child();
+
+        enter_last_child();
+        node.target_type->accept(*this);
+        leave_child();
+    }
+
+    void MyVisitor::visit(ast::Not &node) {
+        print_indented("Not");
+
+        enter_last_child();
+        node.exp->accept(*this);
+        leave_child();
+    }
+
+    void MyVisitor::visit(ast::And &node) {
+        print_indented("And");
+
+        enter_child();
+        node.left->accept(*this);
+        leave_child();
+
+        enter_last_child();
+        node.right->accept(*this);
+        leave_child();
+    }
+
+    void MyVisitor::visit(ast::Or &node) {
+        print_indented("Or");
+
+        enter_child();
+        node.left->accept(*this);
+        leave_child();
+
+        enter_last_child();
+        node.right->accept(*this);
+        leave_child();
+    }
+
+    void MyVisitor::visit(ast::ExpList &node) {
+        print_indented("ExpList");
+
+        for (auto it = node.exps.rbegin(); it != node.exps.rend(); ++it) {
+            if (it != node.exps.rend() - 1) {
+                enter_child();
+            } else {
+                enter_last_child();
+            }
+            (*it)->accept(*this);
+            leave_child();
+        }
+    }
+
+    void MyVisitor::visit(ast::Call &node) {
+        print_indented("Call");
+
+        enter_child();
+        node.func_id->accept(*this);
+        leave_child();
+
+        enter_last_child();
+        node.args->accept(*this);
+        leave_child();
+    }
+
+    void MyVisitor::visit(ast::Statements &node) {
+        print_indented("Statements");
+
+        for (auto it = node.statements.begin(); it != node.statements.end(); ++it) {
+            if (it != node.statements.end() - 1) {
+                enter_child();
+            } else {
+                enter_last_child();
+            }
+            (*it)->accept(*this);
+            leave_child();
+        }
+    }
+
+    void MyVisitor::visit(ast::Break &node) {
+        print_indented("Break");
+    }
+
+    void MyVisitor::visit(ast::Continue &node) {
+        print_indented("Continue");
+    }
+
+    void MyVisitor::visit(ast::Return &node) {
+        print_indented("Return");
+
+        if (node.exp) {
+            enter_last_child();
+            node.exp->accept(*this);
+            leave_child();
+        }
+    }
+
+    void MyVisitor::visit(ast::If &node) {
+        print_indented("If");
+
+        enter_child();
+        node.condition->accept(*this);
+        leave_child();
+
+        if (node.otherwise) {
+            enter_child();
+        } else {
+            enter_last_child();
+        }
+        node.then->accept(*this);
+        leave_child();
+
+        if (node.otherwise) {
+            enter_last_child();
+            node.otherwise->accept(*this);
+            leave_child();
+        }
+    }
+
+    void MyVisitor::visit(ast::While &node) {
+        print_indented("While");
+
+        enter_child();
+        node.condition->accept(*this);
+        leave_child();
+
+        enter_last_child();
+        node.body->accept(*this);
+        leave_child();
+    }
+
+    void MyVisitor::visit(ast::VarDecl &node) {
+        node.id->accept(*this);
+        node.type->accept(*this);
+        if (node.init_exp) {
+            node.init_exp->accept(*this);
+            if()
+        }
+        if(id_exists(node.id->value)) {
+            errorDef(node.line, node.id->value);
+        }
+        insert_variable(node.id->value, node.type->type);
+    }
+
+    void MyVisitor::visit(ast::Assign &node) {
+        print_indented("Assign");
+
+        enter_child();
+        node.id->accept(*this);
+        leave_child();
+
+        enter_last_child();
+        node.exp->accept(*this);
+        leave_child();
+    }
+
+    void MyVisitor::visit(ast::Formal &node) {
+        print_indented("Formal");
+
+        enter_child();
+        node.id->accept(*this);
+        leave_child();
+
+        enter_last_child();
+        node.type->accept(*this);
+        leave_child();
+    }
+
+    void MyVisitor::visit(ast::Formals &node) {
+        print_indented("Formals");
+
+        for (auto it = node.formals.rbegin(); it != node.formals.rend(); ++it) {
+            if (it != node.formals.rend() - 1) {
+                enter_child();
+            } else {
+                enter_last_child();
+            }
+            (*it)->accept(*this);
+            leave_child();
+        }
+    }
+
+    void MyVisitor::visit(ast::FuncDecl &node) {
+        print_indented("FuncDecl");
+
+        enter_child();
+        node.id->accept(*this);
+        leave_child();
+
+        enter_child();
+        node.return_type->accept(*this);
+        leave_child();
+
+        enter_child();
+        node.formals->accept(*this);
+        leave_child();
+
+        enter_last_child();
+        node.body->accept(*this);
+        leave_child();
+    }
+
+    void MyVisitor::visit(ast::Funcs &node) {
+        print_indented("Funcs");
+
+        for (auto it = node.funcs.begin(); it != node.funcs.end(); ++it) {
+            if (it != node.funcs.end() - 1) {
+                enter_child();
+            } else {
+                enter_last_child();
+            }
+            (*it)->accept(*this);
+            leave_child();
+        }
+    }
+
+    void MyVisitor :: insert_variable(std::string name, ast::BuiltInType type)
+    {
+        int offset = this->offset_table.back();
+        std::vector<ast::BuiltInType> dummy;
+        SymTableEntry element = {name, type, dummy, offset};
+        this->sym_table.back().push_back(element);
+        this->scope_printer.emitVar(name, type, offset);
+        this->offset_table.back()+= SIZE_OF_TYPE;
+    }
+
+    void MyVisitor :: insert_func(std::string name, ast::BuiltInType return_type , std::vector<ast::BuiltInType> params)
+    {
+        SymTableEntry element = {name, return_type, params, 0};
+        this->sym_table.front().push_back(element);
+        this->scope_printer.emitFunc(name, return_type, params);
+    }
+
+    void MyVisitor :: begin_Scope()
+    {
+        std::vector<SymTableEntry> initial_vector;
+        this->sym_table.push_back(initial_vector);
+        this->offset_table.push_back(this->offset_table.back());
+        this->scope_printer.beginScope();
+    }
+
+    void MyVisitor :: end_scope()
+    {
+        this->sym_table.pop_back();
+        this->offset_table.pop_back();
+        this->scope_printer.endScope();
+
+    }
+
+    const SymTableEntry * MyVisitor :: id_exists(std::string name)
+    {
+        for(auto const & s : this->sym_table){
+            for(auto const &d : s){
+                if(d.name == name){
+                    return &d;
+                }
+            }
+        }
+
+        return nullptr;
+    }
+
+
 }
+
--- HW3/output.hpp
+++ HW3/output.hpp
@@ -2,11 +2,14 @@
 #define OUTPUT_HPP
 
 #include <vector>
+#include <map>
+
 #include <string>
 #include <sstream>
 #include "visitor.hpp"
 #include "nodes.hpp"
 
+const unsigned int SIZE_OF_TYPE = 1;
 namespace output {
     /* Error handling functions */
 
@@ -44,7 +47,7 @@ namespace output {
         std::stringstream globalsBuffer;
         std::stringstream buffer;
         int indentLevel;
-
+        
         std::string indent() const;
 
     public:
@@ -61,6 +64,101 @@ namespace output {
 
         friend std::ostream &operator<<(std::ostream &os, const ScopePrinter &printer);
     };
+
+    typedef struct
+    {
+        std::string name;
+        ast::BuiltInType ret_type;
+        std::vector<ast::BuiltInType> paramTypes;
+        int offset;
+    }  SymTableEntry ;
+    
+    
+    typedef struct
+    {
+        // TO DO
+    }  TypeTableEntry ;
+    
+    class MyVisitor : public Visitor {
+    private:
+        std::vector<std::vector<SymTableEntry>> sym_table;
+        std::vector<int> offset_table;
+        std::map<std::string, TypeTableEntry> type_table;
+        ScopePrinter scope_printer;
+
+        /* Helper function to print a string with the current indentation */
+        //void print_indented(const std::string &str);
+
+        /* Functions to manage the indentation level */
+        // void enter_child();
+
+        // void enter_last_child();
+
+        // void leave_child();
+        void insert_variable(std::string name, ast::BuiltInType type);
+        void insert_func(std::string name, ast::BuiltInType return_type , std::vector<ast::BuiltInType> params);
+        void begin_Scope();
+        void end_scope();
+        const SymTableEntry* id_exists(std::string name);
+       // bool resolved_forward_references();
+
+    public:
+        MyVisitor();
+
+        void visit(ast::Num &node) override;
+
+        void visit(ast::NumB &node) override;
+
+        void visit(ast::String &node) override;
+
+        void visit(ast::Bool &node) override;
+
+        void visit(ast::ID &node) override;
+
+        void visit(ast::BinOp &node) override;
+
+        void visit(ast::RelOp &node) override;
+
+        void visit(ast::Not &node) override;
+
+        void visit(ast::And &node) override;
+
+        void visit(ast::Or &node) override;
+
+        void visit(ast::Type &node) override;
+
+        void visit(ast::Cast &node) override;
+
+        void visit(ast::ExpList &node) override;
+
+        void visit(ast::Call &node) override;
+
+        void visit(ast::Statements &node) override;
+
+        void visit(ast::Break &node) override;
+
+        void visit(ast::Continue &node) override;
+
+        void visit(ast::Return &node) override;
+
+        void visit(ast::If &node) override;
+
+        void visit(ast::While &node) override;
+
+        void visit(ast::VarDecl &node) override;
+
+        void visit(ast::Assign &node) override;
+
+        void visit(ast::Formal &node) override;
+
+        void visit(ast::Formals &node) override;
+
+        void visit(ast::FuncDecl &node) override;
+
+        void visit(ast::Funcs &node) override;
+    };
 }
+
+
 
 #endif //OUTPUT_HPP
